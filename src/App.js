bbee462a03ec7fc6940b4bbcd90495933f3e03b4
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-18 17:39:24 -0300

    created state variable for new posts

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Post from './Post';
 import Header from './Header';
 
-const posts = [
-  { title: 'Title 1', subtitle: 'Subtitle 1', likes: 10 },
-  { title: 'Title 2', subtitle: 'Subtitle 2', likes: 20 },
-  { title: 'Title 3', subtitle: 'Subtitle 3', likes: 30 },
-];
 function App() {
+  const [posts, setPosts] = useState([
+    { title: 'Title 1', subtitle: 'Subtitle 1', likes: 10 },
+    { title: 'Title 2', subtitle: 'Subtitle 2', likes: 20 },
+    { title: 'Title 3', subtitle: 'Subtitle 3', likes: 30 },
+  ]);
+
+  function handleRefresh() {
+    setPosts([
+      ...posts,
+      {
+        title: `Title ${posts.length + 1}`,
+        subtitle: `Subtitle ${posts.length + 1}`,
+        likes: 40,
+      },
+    ]);
+  }
+
   return (
     <>
       <Header>
-        <h2>Posts of the week</h2>
+        <h2>
+          Posts of the week <button onClick={handleRefresh}>Refresh</button>
+        </h2>
       </Header>
       <hr />
       {posts.map(({ title, subtitle, likes }) => (
