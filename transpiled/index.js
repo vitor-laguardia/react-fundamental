ebcf255edb8a00c58cf4ec93c3a597f0a6d62188
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-18 12:09:31 -0300

    used babel to transpile index file in transpiled folder. used this folder to entry point of webpack config.

---
+++ transpiled/index.js
@@ -0,0 +1,6 @@
+"use strict";
+
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { "default": obj }; }
+_reactDom["default"].render( /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World!"), document.getElementById('root'));
