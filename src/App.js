ca2d8eefe4cd3496303a6c553a414597beb6f7a7
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-19 20:20:49 -0300

    added theme provider in main app ad created posts objects

--- src/App.js
+++ src/App.js
@@ -1,12 +1,30 @@
 import React, { useState } from 'react';
 import Post from './Post';
 import Header from './Header';
-
+import ThemeProvider from './ThemeProvider';
 function App() {
   const [posts, setPosts] = useState([
-    { id: Math.random(), title: 'Title 1', subtitle: 'Subtitle 1', likes: 10 },
-    { id: Math.random(), title: 'Title 2', subtitle: 'Subtitle 2', likes: 20 },
-    { id: Math.random(), title: 'Title 3', subtitle: 'Subtitle 3', likes: 30 },
+    {
+      id: Math.random(),
+      title: 'Title 1',
+      subtitle: 'Subtitle 1',
+      likes: 10,
+      read: true,
+    },
+    {
+      id: Math.random(),
+      title: 'Title 2',
+      subtitle: 'Subtitle 2',
+      likes: 20,
+      read: false,
+    },
+    {
+      id: Math.random(),
+      title: 'Title 3',
+      subtitle: 'Subtitle 3',
+      likes: 30,
+      read: true,
+    },
   ]);
 
   function handleRefresh() {
@@ -17,6 +35,7 @@ function App() {
         title: `Title ${prevPost.length + 1}`,
         subtitle: `Subtitle ${prevPost.length + 1}`,
         likes: 40,
+        read: false,
       },
     ]);
   }
@@ -26,22 +45,17 @@ function App() {
   }
 
   return (
-    <>
+    <ThemeProvider>
       <Header>
         <h2>
           Posts of the week <button onClick={handleRefresh}>Refresh</button>
         </h2>
       </Header>
       <hr />
-      {posts.map(({ id, title, subtitle, likes }) => (
-        <Post
-          key={title}
-          post={{ id, title, subtitle }}
-          likes={likes}
-          onRemove={handleRemovePost}
-        />
+      {posts.map((post) => (
+        <Post key={post.id} post={post} onRemove={handleRemovePost} />
       ))}
-    </>
+    </ThemeProvider>
   );
 }
 
