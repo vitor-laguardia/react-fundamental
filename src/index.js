2cbf6d83da0dda8ab79781d15a7ec90d36dbcd36
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-26 11:01:57 -0300

    added globalstyle in index file

--- src/index.js
+++ src/index.js
@@ -1,5 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/App';
+import GlobalStyle from './styles/global';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+  <>
+    <App />
+    <GlobalStyle />
+  </>,
+  document.getElementById('root')
+);
