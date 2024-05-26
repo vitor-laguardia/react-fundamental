aface26b29841cf3e45f638791dbb5070716260f
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-26 11:04:04 -0300

    used title style component

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,12 +2,17 @@ import React, { useContext } from 'react';
 import PropTypes from 'prop-types';
 import Button from '../Button';
 import { ThemeContext } from '../../contexts/ThemeProvider';
+import styled from 'styled-components';
+import Title from '../Title';
+// const Title = styled.h1`
+//   color: #637bf3;
+// `;
 
 export default function Header({ title, children }) {
   const { onToggleTheme } = useContext(ThemeContext);
   return (
     <>
-      <h1>{title}</h1>
+      <Title>{title}</Title>
       <Button onClick={onToggleTheme}>Change Theme</Button>
       {children}
     </>
