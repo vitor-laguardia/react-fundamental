dcc14718a4fcb0f01dc80033a0a505c016a20998
vitor laguardia <vitor.laguardia@hotmail.com>
2024-05-26 11:05:26 -0300

    created globalstyle file in style components

---
+++ src/styles/global.js
@@ -0,0 +1,10 @@
+import { createGlobalStyle } from 'styled-components';
+
+//margins reset, etc
+export default createGlobalStyle`
+  body {
+    background: #222;
+    font-family: sans-serif;
+    color: #fff
+  }
+`;
